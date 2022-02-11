b3b8b75fae3ae28c617da5d40fa941b52e38560e
Yuriiru <98612792+Yuriiru@users.noreply.github.com>
2022-02-11 20:15:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int a = 1; a <= 10; a++) {
+                System.out.println(i + " * " + a + " = " + i * a);
+            }
+            System.out.println();
+        }
+    }
+}
